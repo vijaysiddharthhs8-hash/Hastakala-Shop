c3042aeb7d492db3315efc445d17c8fb217264b4
Vijay Siddharth.H.S <vijaysiddharthhs8@gmail.com>
2026-05-18 21:22:59 +0530

    Add files via upload

---
+++ app/build/generated/source/buildConfig/androidTest/debug/com/hastakalashop/app/test/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.hastakalashop.app.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.hastakalashop.app.test";
+  public static final String BUILD_TYPE = "debug";
+  // Field from default config.
+  public static final String GEMINI_API_KEY = "";
+}
